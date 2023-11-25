0088eec603e61952decf7511812f14b2ec3fec5f
Hidilson45 <hidilson45@hotmail.com>
2023-11-25 19:27:39 +0000

    ggg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Header from '@/components/Header'
-
+import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Image from 'next/image'
 
@@ -8,7 +8,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col bg-[#121212]'>
       <Header/>
       <div className='container px-12 py-4 mt-12'>
-        
+        <Hero/>
         <About/>
       </div>
     </main>
