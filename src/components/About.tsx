5f4fbe030e92c976c29e675df906509333862bb1
Hidilson45 <hidilson45@hotmail.com>
2024-01-25 14:48:14 +0000

    Get

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,7 +11,7 @@ export default function About(){
                 <Image src={AboutImage} alt='ImageAbout' width={400} height={400}></Image>
                 <div>
                     <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-                    <p className='text-base lg:text-lg'>O Chelsea Football Club é um dos maiores clubes de futebol da Inglaterra, sediado na cidade de Londres, fundado em 10 de março de 1905. Internacionalmente, é um dos três clubes mais vitoriosos do futebol inglês, ao lado do Liverpool e Manchester United
+                    <p className='text-base lg:text-lg'>
                     </p>
                     <div className='flex flex-row mt-8'>
                     <span className='mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-blue-500'>Skills</span>
