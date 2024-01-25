dccd326a20ab92072590c19374ee7d073c90c948
Hidilson45 <hidilson45@hotmail.com>
2024-01-25 14:57:22 +0000

    About Update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,7 +11,7 @@ export default function About(){
                 <Image src={AboutImage} alt='ImageAbout' width={400} height={400}></Image>
                 <div>
                     <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-                    <p className='text-base lg:text-lg'>
+                    <p className='text-base lg:text-lg'>I am a dedicated and enthusiastic Software Developer with a deep-rooted passion for back-end development. I have always been fascinated by the intricate workings of systems behind the scenes, which led me to specialize in back-end development. I excel in designing, building, and maintaining efficient code. I value collaboration and am committed to continuous learning. Outside of work, I enjoy staying updated with the latest tech trends and participating in coding challenges.
                     </p>
                     <div className='flex flex-row mt-8'>
                     <span className='mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-blue-500'>Skills</span>
